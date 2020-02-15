c791c3e05af7c4f1a2b07c072075a3cfc87c36d2
repairnator <noreply@github.com>
2020-02-15 14:46:24 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-ButterFaces-ButterFaces-275460275-20170914-151122_bugonly-firstCommit

---
+++ components/src/main/java/de/larmic/butterfaces/model/table/SortType.java
@@ -0,0 +1,10 @@
+package de.larmic.butterfaces.model.table;
+
+/**
+ * Table row sort type (used by {@link TableRowSortingModel}.
+ */
+public enum SortType {
+
+    NONE, ASCENDING, DESCENDING;
+
+}
