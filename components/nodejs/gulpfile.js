91023891edee73bee05a7c8b816df367f2cf2a03
repairnator <noreply@github.com>
2020-02-15 14:46:22 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-ButterFaces-ButterFaces-285584704-20171009-155025-firstCommit

---
+++ components/nodejs/gulpfile.js
@@ -0,0 +1,322 @@
+/*
+ Recipes for using Gulp:
+ https://github.com/gulpjs/gulp/tree/master/docs/recipes
+ */
+
+// DEPENDENCIES ===============================================================================
+
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var merge = require("merge-stream");
+var bower = require("gulp-bower");
+var tsd = require("gulp-tsd");
+var ts = require("gulp-typescript");
+var concat = require("gulp-concat");
+var sourcemaps = require("gulp-sourcemaps");
+var del = require("del");
+var pipe = require("multipipe");
+var less = require("gulp-less");
+var mirror = require("gulp-mirror");
+var rename = require("gulp-rename");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer-core");
+var tslint = require("gulp-tslint");
+var uglify = require("gulp-uglify");
+var UglifyJS = require("uglify-js");
+var minifyCSS = require("gulp-minify-css");
+var gzip = require("gulp-gzip");
+var stripDebug = require("gulp-strip-debug");
+var sizereport = require("gulp-sizereport");
+
+// CONSTANTS ===============================================================================
+
+var RESSOURCE_DIR = "../src/main/resources/META-INF/resources";
+
+var paths = {
+    bower: {
+        root: "./bower_components/",
+        jquery: "./bower_components/jquery/dist/**/*.{js,map}",
+        jqueryinputmask: "./bower_components/jquery.inputmask/dist/jquery.inputmask.bundle.js",
+        prettify: "./bower_components/google-code-prettify/src/prettify.{js,css}",
+        bootstrap_css: "./bower_components/bootstrap/dist/css/bootstrap.{css,css.map}",
+        bootstrap_js: "./bower_components/bootstrap/dist/js/bootstrap.{js,map}",
+        glyphicons_css: "./bower_components/glyphicons/styles/glyphicons.css",
+        glyphicon_fonts: "./bower_components/glyphicons/fonts/**/*.*",
+        popperjs: "./bower_components/popper.js/dist/umd/popper.js",
+        momentjs: "./bower_components/moment/min/moment-with-locales.js",
+        tempusdominus_core_js: "./bower_components/tempusdominus-core/build/js/tempusdominus-core.min.js",
+        tempusdominus_bootstrap_js: "./bower_components/tempusdominus-bootstrap-4/build/js/tempusdominus-bootstrap-4.min.js",
+        tempusdominus_bootstrap_css: "./bower_components/tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.min.css"
+    },
+    source: {
+        typescripts: RESSOURCE_DIR + "/butterfaces-ts/**/*.ts",
+        javascript: RESSOURCE_DIR + "/butterfaces-js/**/*.js",
+        less: RESSOURCE_DIR + "/butterfaces-less/*.less"
+    },
+    destination: {
+        root: RESSOURCE_DIR + "/butterfaces-dist",
+        css: RESSOURCE_DIR + "/butterfaces-dist-css",
+        js: RESSOURCE_DIR + "/butterfaces-dist-js",
+        external: RESSOURCE_DIR + "/butterfaces-external",
+        bundle_js: RESSOURCE_DIR + "/butterfaces-dist-bundle-js",
+        bower: RESSOURCE_DIR + "/butterfaces-dist-bower",
+        bower_font: RESSOURCE_DIR + "/fonts",
+        ts_external_definitions: RESSOURCE_DIR + "/butterfaces-ts/definitions/external"
+    }
+};
+
+// DIST GOALS ===============================================================================
+
+gulp.task("clean", function (cb) {
+    del([
+            paths.destination.css,
+            paths.destination.js,
+            paths.destination.bower,
+            paths.destination.bower_font,
+            paths.destination.bundle_js,
+            paths.destination.ts_external_definitions
+        ],
+        {force: true}, cb);
+});
+
+gulp.task("bower:loadDependencies", function () {
+    // https://github.com/bower/bower/issues/1019#issuecomment-52700170
+    return bower({force: false})
+        .pipe(gulp.dest(paths.bower.root));
+});
+
+gulp.task("bower:copyDependenciesToDist", ["bower:loadDependencies"], function () {
+    var copyDependenciesToDist = gulp.src([
+            paths.bower.jquery,
+            paths.bower.jqueryinputmask,
+            paths.bower.prettify,
+            paths.bower.bootstrap_css,
+            paths.bower.bootstrap_js,
+            paths.bower.glyphicons_css,
+            paths.bower.popperjs,
+            paths.bower.momentjs,
+            paths.bower.tempusdominus_core_js,
+            paths.bower.tempusdominus_bootstrap_css,
+            paths.bower.tempusdominus_bootstrap_js
+        ])
+        .pipe(gulp.dest(paths.destination.bower));
+
+    var copyFontDependenciesToDist = gulp.src([
+            paths.bower.glyphicon_fonts
+        ])
+        .pipe(gulp.dest(paths.destination.bower_font));
+
+    return merge(copyDependenciesToDist, copyFontDependenciesToDist);
+});
+
+gulp.task("typescript:loadDefinitions", function (callback) {
+    tsd({
+        command: "reinstall",
+        config: "./tsd.json"
+    }, callback);
+});
+
+gulp.task("typescript:lint", ["typescript:loadDefinitions"], function () {
+    return gulp.src([
+            paths.source.typescripts,
+            "!" + paths.destination.ts_external_definitions + "/**/*.ts"
+        ])
+        .pipe(tslint({
+            formatter: "verbose"
+        }))
+        .pipe(tslint.report())
+});
+
+gulp.task("typescript:compileToBundle", ["typescript:lint"], function () {
+    var tsResult = gulp.src(paths.source.typescripts)
+        .pipe(ts({
+            noImplicitAny: true,
+            target: "es5"
+        }));
+
+    return tsResult.js
+        .pipe(concat("butterfaces-ts-bundle.js"))
+        .pipe(stripDebug())
+        .pipe(mirror(
+            pipe(
+                rename(function (path) {
+                    path.basename += ".min";
+                }),
+                uglify()
+            )
+        ))
+        .pipe(gulp.dest(paths.destination.bundle_js));
+});
+
+gulp.task("typescript:compileToSingleFiles", ["typescript:lint"], function () {
+    var tsResult = gulp.src(paths.source.typescripts)
+        .pipe(sourcemaps.init())
+        .pipe(ts({
+            noImplicitAny: true,
+            target: "es5"
+        }));
+
+    return tsResult.js
+        .pipe(mirror(
+            pipe(
+                rename(function (path) {
+                    path.basename += ".min";
+                }),
+                uglify()
+            )
+        ))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.destination.js));
+});
+
+gulp.task("less:compile", ["bower:copyDependenciesToDist"], function () {
+    return gulp.src([paths.source.less])
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(postcss([autoprefixer({browsers: ["> 2%"]})]))
+        .pipe(mirror(
+            pipe(
+                rename(function (path) {
+                    path.basename += ".min";
+                }),
+                minifyCSS()
+            )
+        ))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.destination.css));
+});
+
+gulp.task("javascript:buildComponentsBundle", function () {
+    return gulp.src(paths.source.javascript)
+        .pipe(concat("butterfaces-js-bundle.js"))
+        .pipe(stripDebug())
+        .pipe(mirror(
+            pipe(
+                rename(function (path) {
+                    path.basename += ".min";
+                }),
+                uglify()
+            )
+        ))
+        .pipe(gulp.dest(paths.destination.bundle_js));
+});
+
+gulp.task("compileResources", ["less:compile", "typescript:compileToBundle", "typescript:compileToSingleFiles", "javascript:buildComponentsBundle"]);
+
+gulp.task("javascript:buildAllBundle", ["compileResources"], function () {
+    var buildButterFacesOnlyBunde = gulp.src([
+            paths.destination.bower + "/prettify.js",
+            paths.destination.bower + "/moment-with-locales.js",
+            paths.destination.bower + "/tempusdominus-core.min.js",
+            paths.destination.bower + "/tempusdominus-bootstrap-4.min.js",
+            paths.destination.bower + "/jquery.inputmask.bundle.js",
+            paths.destination.external + "/*.js",
+            paths.destination.bundle_js + "/butterfaces-ts-bundle.min.js",
+            paths.destination.bundle_js + "/butterfaces-js-bundle.min.js"
+        ])
+        .pipe(stripDebug())
+        .pipe(uglify())
+        .pipe(concat("butterfaces-all-bundle.min.js"))
+        .pipe(gulp.dest(paths.destination.bundle_js));
+
+    var buildAllWithJQueryBundle = gulp.src([
+            paths.destination.bower + "/jquery.min.js",
+            paths.destination.bower + "/prettify.js",
+            paths.destination.bower + "/moment-with-locales.js",
+            paths.destination.bower + "/tempusdominus-core.min.js",
+            paths.destination.bower + "/tempusdominus-bootstrap-4.min.js",
+            paths.destination.bower + "/jquery.inputmask.bundle.js",
+            paths.destination.external + "/*.js",
+            paths.destination.bundle_js + "/butterfaces-ts-bundle.min.js",
+            paths.destination.bundle_js + "/butterfaces-js-bundle.min.js"
+        ])
+        .pipe(stripDebug())
+        .pipe(uglify())
+        .pipe(concat("butterfaces-all-with-jquery-bundle.min.js"))
+        .pipe(gulp.dest(paths.destination.bundle_js));
+
+    var buildAllWithBootstrapBundle = gulp.src([
+            paths.destination.bower + "/prettify.js",
+            paths.destination.bower + "/moment-with-locales.js",
+            paths.destination.bower + "/tempusdominus-core.min.js",
+            paths.destination.bower + "/tempusdominus-bootstrap-4.min.js",
+            paths.destination.bower + "/popper.js",
+            paths.destination.bower + "/bootstrap.js",
+            paths.destination.bower + "/jquery.inputmask.bundle.js",
+            paths.destination.external + "/*.js",
+            paths.destination.bundle_js + "/butterfaces-ts-bundle.min.js",
+            paths.destination.bundle_js + "/butterfaces-js-bundle.min.js"
+        ])
+        .pipe(stripDebug())
+        .pipe(uglify())
+        .pipe(concat("butterfaces-all-with-bootstrap-bundle.min.js"))
+        .pipe(gulp.dest(paths.destination.bundle_js));
+
+    var buildAllWithJQueryAndBootstrapBundle = gulp.src([
+            paths.destination.bower + "/jquery.min.js",
+            paths.destination.bower + "/moment-with-locales.js",
+            paths.destination.bower + "/tempusdominus-core.min.js",
+            paths.destination.bower + "/tempusdominus-bootstrap-4.min.js",
+            paths.destination.bower + "/prettify.js",
+            paths.destination.bower + "/popper.js",
+            paths.destination.bower + "/bootstrap.js",
+            paths.destination.bower + "/jquery.inputmask.bundle.js",
+            paths.destination.external + "/*.js",
+            paths.destination.bundle_js + "/butterfaces-ts-bundle.min.js",
+            paths.destination.bundle_js + "/butterfaces-js-bundle.min.js"
+        ])
+        .pipe(concat("butterfaces-all-with-jquery-and-bootstrap-bundle.min.js"))
+        .pipe(stripDebug())
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.destination.bundle_js));
+
+    return merge(buildButterFacesOnlyBunde, buildAllWithJQueryBundle, buildAllWithBootstrapBundle, buildAllWithJQueryAndBootstrapBundle);
+});
+
+gulp.task("dist:zip", ["javascript:buildAllBundle"], function () {
+    return gulp.src([
+            paths.destination.css + "/**/*",
+            paths.destination.js + "/**/*",
+            paths.destination.bower + "/**/*",
+            paths.destination.bundle_js + "/**/*",
+            "!" + paths.destination.css + "/**/*.gz",
+            "!" + paths.destination.js + "/**/*.gz",
+            "!" + paths.destination.bower + "/**/*.gz",
+            "!" + paths.destination.bundle_js + "/**/*.gz"
+        ], {base: "."})
+        .pipe(gzip())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("sizereport:css", function () {
+    return gulp.src([
+            paths.destination.css + "/*.css",
+            "!" + paths.destination.css + "/*.sourcemaps.css"
+        ])
+        .pipe(sizereport({gzip: true}));
+});
+
+gulp.task("sizereport:js", function () {
+    return gulp.src([
+            paths.destination.js + "/*.js",
+            "!" + paths.destination.js + "/*.min.js",
+        ])
+        .pipe(sizereport({
+            gzip: true,
+            minifier: function (contents, filepath) {
+                if (filepath.match(/\.min\./g)) {
+                    return contents;
+                }
+                return UglifyJS.minify(contents, {fromString: true}).code;
+            }
+        }));
+});
+
+// MAIN GOALS ===============================================================================
+
+gulp.task("dist:build", ["compileResources", "dist:zip"]);
+
+gulp.task("sizereports", ["sizereport:js", "sizereport:css", "dist:build"]);
+
+// gulp.task("default", ["dist:build", "sizereports"]);
+gulp.task("default", ["dist:build"]);
---
+++ readme.md
@@ -0,0 +1,20 @@
+# ButterFaces a smart JSF framework [![Build Status](https://travis-ci.org/ButterFaces/ButterFaces.svg?branch=master)](https://travis-ci.org/ButterFaces/ButterFaces)
+
+## What is ButterFaces?
+
+YAJF - yet another JSF Framework. There are a lot of JSF frameworks like [RichFaces](http://www.richfaces.org) or [PrimeFaces](http://www.http://primefaces.org/). Most of them brings a lot of features and they feel like fat clients. ButterFaces is a lightweight JSF framework bringing a lot of nice features to JSF with beeing minimal invasive. Easy to use and easy to extend by custom features.
+<br/>
+<br/>
+ButterFaces is a JSF 2 component enrichment framework. It wrappes most of default JSF 2 components and adds labels, readonly-views, tooltips, placeholder, validation style class marker and other nice features to the standard JSF 2 components.
+
+## Why ButterFaces?
+It is simple: Because everything tastes better with butter. Plain JSF brings a lot of components but if you want to use them they are naked. There are no labels for input components, no tooltips except of titles and html5 is not supported. If you want to create a template supporting input and output components you have to implement both types of components.
+<br/>
+<br/>
+ButterFaces creates a simple html structure based on [twitters bootstrap](http://www.http://getbootstrap.com/) around existing JSF components. You do not have already use bootstrap in your project - it comes with ButterFaces. And if you do not want to use bootstrap styles or you have your own version you can disable it easily by configuring it in your web.xml.
+
+## What about the license?
+ButterFaces stands under [MIT License](http://opensource.org/licenses/MIT). So feel free to use it. Feel free to customize it. Feel free to sell it. But let us know how it works and what kind of problems you have.
+
+## What about a documentation?
+Take a look at our [showcase](http://www.butterfaces.org/) or [documentation](https://butterfaces.gitbooks.io/butterfaces/content/)
