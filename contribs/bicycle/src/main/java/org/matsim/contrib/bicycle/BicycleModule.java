b1087988eec6defb3814f64cbda7e4d07578d0a6
repairnator <noreply@github.com>
2020-02-16 15:29:48 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-matsim-org-matsim-363442175-20180407-112452-firstCommit

---
+++ contribs/bicycle/src/main/java/org/matsim/contrib/bicycle/BicycleModule.java
@@ -0,0 +1,46 @@
+/* *********************************************************************** *
+ * project: org.matsim.*												   *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package org.matsim.contrib.bicycle;
+
+import org.matsim.core.controler.AbstractModule;
+
+/**
+ * @author smetzler, dziemke
+ */
+public class BicycleModule extends AbstractModule {
+	private boolean considerMotorizedInteraction;
+	
+	@Override
+	public void install() {
+		bind(BicycleTravelTime.class).asEagerSingleton();
+		addTravelTimeBinding("bicycle").to(BicycleTravelTime.class);
+		bind(BicycleTravelDisutilityFactory.class).asEagerSingleton();
+		addTravelDisutilityFactoryBinding("bicycle").to(BicycleTravelDisutilityFactory.class);
+		bindScoringFunctionFactory().toInstance(new BicycleScoringFunctionFactory());
+//		bindScoringFunctionFactory().to(BicycleScoringFunctionFactory.class); // Leads to "Tried proxying org.matsim.core.scoring.ScoringFunctionsForPopulation to support a circular dependency, but it is not an interface."
+		
+		if (considerMotorizedInteraction) {
+			addMobsimListenerBinding().to(MotorizedInteractionEngine.class);
+		}
+	}
+	
+	public void setConsiderMotorizedInteraction(boolean considerMotorizedInteraction) {
+		this.considerMotorizedInteraction = considerMotorizedInteraction;
+	}
+}
