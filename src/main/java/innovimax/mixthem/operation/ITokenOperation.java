6ad053791d0c2e361bf44034e667052bce1c75ef
repairnator <noreply@github.com>
2020-03-17 16:34:12 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-Innovimax-SARL-mix-them-389154834-20180607-112723-firstCommit

---
+++ src/main/java/innovimax/mixthem/operation/ITokenOperation.java
@@ -0,0 +1,25 @@
+
+package innovimax.mixthem.operation;
+
+import innovimax.mixthem.MixException;
+import innovimax.mixthem.io.Token;
+
+import java.util.List;
+
+interface ITokenOperation extends IOperation {
+	/**
+ 	* Processes operation and set new result in the TokenResult parameter.
+	* @param tokenRange The range of tokens to mix	
+	* @param result The previous operation result
+ 	* @return The result of the operation (maybe null)
+ 	* @throws MixException - If an mixing error occurs
+	* @see innovimax.mixthem.operation.TokenResult	
+	*/	
+	void process(List<Token> tokenRange, TokenResult result) throws MixException;
+	/**
+ 	* Is mixing operation is  possible on token range?
+	* @param tokenRange The range of tokens to mix		
+ 	* @return True if mixing operation is possible 	
+	*/	
+	boolean mixable(List<Token> tokenRange);
+}
