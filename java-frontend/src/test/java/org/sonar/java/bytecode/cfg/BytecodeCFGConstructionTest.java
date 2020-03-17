2d0cf15bda176d48b786d3c2abdd2424d2225a45
repairnator <noreply@github.com>
2020-03-17 16:37:55 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-SonarSource-sonar-java-264779961-20170815-170800_bugonly-firstCommit

---
+++ java-frontend/src/test/java/org/sonar/java/bytecode/cfg/BytecodeCFGConstructionTest.java
@@ -0,0 +1,229 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.bytecode.cfg;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.util.Printer;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.objectweb.asm.Opcodes.*;
+
+@RunWith(Parameterized.class)
+public class BytecodeCFGConstructionTest {
+
+  public static final String JAVA_LANG_OBJECT = "java/lang/Object";
+
+  @Parameters(name = "{0}")
+  public static Collection<Object[]> data() {
+    ImmutableList.Builder<Object[]> testData = ImmutableList.builder();
+
+    // Instructions without operand
+    testData.addAll(
+      Instructions.NO_OPERAND_INSN.stream()
+        .map(opcode -> new Object[] {new TestInput(opcode), new BytecodeCFGBuilder.Instruction(opcode)})
+        .collect(Collectors.toList()));
+
+    // Instructions with int operand
+    testData.add(new Object[] {intOp(BIPUSH, 1), inst(BIPUSH)});
+    testData.add(new Object[] {intOp(SIPUSH, 1), inst(SIPUSH)});
+    testData.add(new Object[] {intOp(NEWARRAY, 1), inst(NEWARRAY)});
+
+    // LOAD STORE
+    testData.add(new Object[] {intOp(ILOAD, 42), inst(ILOAD, 42)});
+    testData.add(new Object[] {intOp(LLOAD, 42), inst(LLOAD, 42)});
+    testData.add(new Object[] {intOp(FLOAD, 42), inst(FLOAD, 42)});
+    testData.add(new Object[] {intOp(DLOAD, 42), inst(DLOAD, 42)});
+    testData.add(new Object[] {intOp(ALOAD, 42), inst(ALOAD, 42)});
+    testData.add(new Object[] {intOp(ISTORE, 42),inst(ISTORE, 42)});
+    testData.add(new Object[] {intOp(LSTORE, 42),inst(LSTORE, 42)});
+    testData.add(new Object[] {intOp(FSTORE, 42),inst(FSTORE, 42)});
+    testData.add(new Object[] {intOp(DSTORE, 42),inst(DSTORE, 42)});
+    testData.add(new Object[] {intOp(ASTORE, 42),inst(ASTORE, 42)});
+    testData.add(new Object[] {intOp(RET, 42), inst(RET, 42)});
+
+    // Instructions with type argument
+    testData.add(new Object[] {new TestInput(NEW, JAVA_LANG_OBJECT), null});
+    testData.add(new Object[] {new TestInput(ANEWARRAY, JAVA_LANG_OBJECT), null});
+    testData.add(new Object[] {new TestInput(CHECKCAST, JAVA_LANG_OBJECT), null});
+    testData.add(new Object[] {new TestInput(INSTANCEOF, JAVA_LANG_OBJECT), null});
+
+    // Instructions with field argument
+    testData.add(new Object[] {new TestInput(GETSTATIC, new FieldOrMethod(JAVA_LANG_OBJECT, "field", "")), null});
+    testData.add(new Object[] {new TestInput(PUTSTATIC, new FieldOrMethod(JAVA_LANG_OBJECT, "field", "")), null});
+    testData.add(new Object[] {new TestInput(GETFIELD, new FieldOrMethod(JAVA_LANG_OBJECT, "field", "")), null});
+    testData.add(new Object[] {new TestInput(PUTFIELD, new FieldOrMethod(JAVA_LANG_OBJECT, "field", "")), null});
+
+    // Instructions with method argument
+    testData.add(new Object[] {new TestInput(INVOKESPECIAL, new FieldOrMethod(JAVA_LANG_OBJECT, "field", "")), null});
+    testData.add(new Object[] {new TestInput(INVOKESTATIC, new FieldOrMethod(JAVA_LANG_OBJECT, "field", "")), null});
+    testData.add(new Object[] {new TestInput(INVOKEVIRTUAL, new FieldOrMethod(JAVA_LANG_OBJECT, "field", "")), null});
+    testData.add(new Object[] {new TestInput(INVOKEINTERFACE, new FieldOrMethod(JAVA_LANG_OBJECT, "field", "")), null});
+
+    // Jump instructions
+    testData.add(new Object[] {new TestInput(IFEQ), null});
+    testData.add(new Object[] {new TestInput(IFNE), null});
+    testData.add(new Object[] {new TestInput(IFLT), null});
+    testData.add(new Object[] {new TestInput(IFGE), null});
+    testData.add(new Object[] {new TestInput(IFGT), null});
+    testData.add(new Object[] {new TestInput(IFLE), null});
+    testData.add(new Object[] {new TestInput(IF_ICMPEQ), null});
+    testData.add(new Object[] {new TestInput(IF_ICMPNE), null});
+    testData.add(new Object[] {new TestInput(IF_ICMPLT), null});
+    testData.add(new Object[] {new TestInput(IF_ICMPGE), null});
+    testData.add(new Object[] {new TestInput(IF_ICMPGT), null});
+    testData.add(new Object[] {new TestInput(IF_ICMPLE), null});
+    testData.add(new Object[] {new TestInput(IF_ACMPEQ), null});
+    testData.add(new Object[] {new TestInput(IF_ACMPNE), null});
+    testData.add(new Object[] {new TestInput(GOTO), null});
+    testData.add(new Object[] {new TestInput(JSR), null});
+    testData.add(new Object[] {new TestInput(IFNULL), null});
+    testData.add(new Object[] {new TestInput(IFNONNULL), null});
+
+    // The rest
+    testData.add(new Object[] {new TestInput(LDC), null});
+    testData.add(new Object[] {new TestInput(IINC), null});
+    testData.add(new Object[] {new TestInput(INVOKEDYNAMIC), null});
+    testData.add(new Object[] {new TestInput(TABLESWITCH), null});
+    testData.add(new Object[] {new TestInput(LOOKUPSWITCH), null});
+    testData.add(new Object[] {new TestInput(MULTIANEWARRAY), null});
+
+    return testData.build();
+  }
+
+  private static TestInput intOp(int opcode, int operand) {
+    return new TestInput(opcode, operand);
+  }
+
+  private static BytecodeCFGBuilder.Instruction inst(int opcode) {
+    return new BytecodeCFGBuilder.Instruction(opcode);
+  }
+  private static BytecodeCFGBuilder.Instruction inst(int opcode, int operand) {
+    return new BytecodeCFGBuilder.Instruction(opcode, operand);
+  }
+
+  static class TestInput {
+    int opcode;
+    int operandOrVar;
+    String type;
+    FieldOrMethod fieldOrMethod;
+
+    @Override
+    public String toString() {
+      return Printer.OPCODES[opcode];
+    }
+
+    TestInput(int opcode) {
+      this.opcode = opcode;
+    }
+
+    TestInput(int opcode, int operandOrVar) {
+      this.opcode = opcode;
+      this.operandOrVar = operandOrVar;
+    }
+
+    TestInput(int opcode, String type) {
+      this.opcode = opcode;
+      this.type = type;
+    }
+
+    TestInput(int opcode, FieldOrMethod fieldOrMethod) {
+      this.opcode = opcode;
+      this.fieldOrMethod = fieldOrMethod;
+    }
+  }
+
+  static class FieldOrMethod {
+    String owner;
+    String name;
+    String desc;
+
+    FieldOrMethod(String owner, String name, String desc) {
+      this.owner = owner;
+      this.name = name;
+      this.desc = desc;
+    }
+  }
+
+  private TestInput testInput;
+  private BytecodeCFGBuilder.Instruction expected;
+
+  @BeforeClass
+  public static void verifyTestData() {
+    List<Integer> opcodes = data().stream().map(data -> ((TestInput) data[0]).opcode).collect(Collectors.toList());
+    assertThat(opcodes).containsAll(Instructions.ASM_OPCODES);
+  }
+
+  public BytecodeCFGConstructionTest(TestInput testInput, BytecodeCFGBuilder.Instruction expected) {
+    this.testInput = testInput;
+    this.expected = expected;
+  }
+
+  @Test
+  public void test() throws Exception {
+    int opcode = testInput.opcode;
+    if (isJumpInstruction(opcode)) {
+      test_jumps();
+      return;
+    }
+    BytecodeCFGBuilder.BytecodeCFG cfg = new Instructions().cfg(testInput.opcode, testInput.operandOrVar);
+    assertThat(cfg.blocks.size()).isEqualTo(2);
+    if(expected == null) {
+      expected = inst(testInput.opcode);
+    }
+    BytecodeCFGBuilder.Instruction actual = cfg.blocks.get(1).instructions.get(0);
+    assertThat(isEquivalentInstruction(actual, expected)).isTrue();
+  }
+
+  private static boolean isJumpInstruction(int opcode) {
+    return Opcodes.IFEQ <= opcode && opcode <= LOOKUPSWITCH && opcode != RET || opcode==IFNULL || opcode==IFNONNULL;
+  }
+
+  private static boolean isEquivalentInstruction(BytecodeCFGBuilder.Instruction i1, BytecodeCFGBuilder.Instruction i2) {
+    return i1.opcode == i2.opcode && Objects.equals(i1.operand, i2.operand);
+  }
+
+  private void test_jumps() {
+    BytecodeCFGBuilder.BytecodeCFG cfg = new Instructions().cfg(testInput.opcode);
+    assertThat(expected).isNull();
+    if(testInput.opcode == TABLESWITCH) {
+      assertThat(cfg.blocks.size()).isEqualTo(5);
+    } else if(testInput.opcode == LOOKUPSWITCH) {
+      assertThat(cfg.blocks.size()).isEqualTo(3);
+    }else {
+      // exit block, jump block, jump-to block, other block
+      assertThat(cfg.blocks.size()).isEqualTo(4);
+      assertThat(cfg.blocks.get(1).instructions).isEmpty();
+      assertThat(cfg.blocks.get(1).terminator().opcode()).isEqualTo(testInput.opcode);
+      assertThat(cfg.blocks.get(2).instructions.get(0).opcode()).isEqualTo(NOP);
+      assertThat(cfg.blocks.get(3).instructions.get(0).opcode()).isEqualTo(ICONST_0);
+    }
+  }
+}
---
+++ java-frontend/src/test/java/org/sonar/java/bytecode/se/BytecodeEGWalkerTest.java
@@ -0,0 +1,80 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.bytecode.se;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+import org.sonar.java.ast.parser.JavaParser;
+import org.sonar.java.bytecode.loader.SquidClassLoader;
+import org.sonar.java.resolve.SemanticModel;
+import org.sonar.java.se.ProgramState;
+import org.sonar.java.se.constraint.ObjectConstraint;
+import org.sonar.java.se.symbolicvalues.SymbolicValue;
+import org.sonar.java.se.xproc.BehaviorCache;
+import org.sonar.java.se.xproc.MethodBehavior;
+import org.sonar.plugins.java.api.semantic.Symbol;
+import org.sonar.plugins.java.api.tree.ClassTree;
+import org.sonar.plugins.java.api.tree.CompilationUnitTree;
+import org.sonar.plugins.java.api.tree.MethodTree;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class BytecodeEGWalkerTest {
+
+  @Test
+  public void generateMethodBehavior() throws Exception {
+    BytecodeEGWalker bytecodeEGWalker = new BytecodeEGWalker(new BehaviorCache(null));
+    SquidClassLoader squidClassLoader = new SquidClassLoader(Lists.newArrayList(new File("target/test-classes"), new File("target/classes")));
+    File file = new File("src/test/java/org/sonar/java/bytecode/se/BytecodeEGWalkerTest.java");
+    CompilationUnitTree tree = (CompilationUnitTree) JavaParser.createParser().parse(file);
+    SemanticModel.createFor(tree, squidClassLoader);
+    Symbol.MethodSymbol symbol = ((MethodTree) ((ClassTree) ((ClassTree) tree.types().get(0)).members().get(1)).members().get(0)).symbol();
+    MethodBehavior methodBehavior = bytecodeEGWalker.getMethodBehavior(symbol, squidClassLoader);
+    assertThat(methodBehavior.yields()).hasSize(2);
+
+    SymbolicValue svFirstArg = new SymbolicValue();
+    SymbolicValue svsecondArg = new SymbolicValue();
+    SymbolicValue svResult = new SymbolicValue();
+
+    List<ObjectConstraint> collect = methodBehavior.yields().stream().map(my -> {
+      Collection<ProgramState> ps = my.statesAfterInvocation(Lists.newArrayList(svFirstArg, svsecondArg), Lists.newArrayList(), ProgramState.EMPTY_STATE, () -> svResult).collect(Collectors.toList());
+      assertThat(ps).hasSize(1);
+      ProgramState next = ps.iterator().next();
+      return next.getConstraint(svResult, ObjectConstraint.class);
+    })
+      .collect(Collectors.toList());
+    assertThat(collect).hasSize(2).containsOnly(ObjectConstraint.NOT_NULL, ObjectConstraint.NULL);
+
+  }
+
+  static class InnerClass {
+    Object fun(boolean a, Object b) {
+      if (b == null) {
+        return null;
+      }
+      return "";
+    }
+  }
+}
