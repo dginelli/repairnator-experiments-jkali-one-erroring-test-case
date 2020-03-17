6422a366267e40e6d99e3ec4f007691bce987e57
repairnator <noreply@github.com>
2020-03-17 16:36:00 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-afonsohmm-SpringBootAPISample-360153017-20180330-074015-firstCommit

---
+++ README.md
@@ -0,0 +1,4 @@
+[![Build Status](https://travis-ci.org/afonsohmm/SpringBootAPISample.svg?branch=master)](https://travis-ci.org/afonsohmm/SpringBootAPISample)
+
+# SpringBootAPISample
+Exemplo de API em Java utilizando Spring Boot
---
+++ src/test/java/br/com/tecnologiabasica/springbootapisample/api/SpringBootApiSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.tecnologiabasica.springbootapisample.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootApiSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
