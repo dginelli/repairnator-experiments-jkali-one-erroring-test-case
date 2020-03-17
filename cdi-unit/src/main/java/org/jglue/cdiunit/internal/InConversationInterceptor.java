29a3851b736104262e2ceec05921c0f49f2038bf
repairnator <noreply@github.com>
2020-03-17 16:34:20 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-BrynCooke-cdi-unit-250710418-20170706-130220_bugonly-firstCommit

---
+++ cdi-unit/src/main/java/org/jglue/cdiunit/internal/InConversationInterceptor.java
@@ -0,0 +1,52 @@
+/*
+ *    Copyright 2011 Bryn Cooke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jglue.cdiunit.internal;
+
+import javax.enterprise.context.Conversation;
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+import org.jglue.cdiunit.InConversationScope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@InConversationScope
+@Interceptor
+public class InConversationInterceptor {
+
+	private static Logger log = LoggerFactory
+			.getLogger(InConversationInterceptor.class);
+
+	@Inject
+	private Conversation conversation;
+
+
+
+
+	@AroundInvoke
+	public Object around(InvocationContext ctx) throws Exception {
+
+		conversation.begin();
+		try {
+			return ctx.proceed();
+		} finally {
+			conversation.end();
+		}
+
+	}
+}
