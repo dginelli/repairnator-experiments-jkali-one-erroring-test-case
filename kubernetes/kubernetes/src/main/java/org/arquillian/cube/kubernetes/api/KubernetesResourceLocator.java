b46e41efd3f0692c59595aa57b6be568288cd51f
repairnator <noreply@github.com>
2020-03-18 10:35:00 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-arquillian-arquillian-cube-284113309-20171006-112228_bugonly-firstCommit

---
+++ kubernetes/kubernetes/src/main/java/org/arquillian/cube/kubernetes/api/KubernetesResourceLocator.java
@@ -0,0 +1,13 @@
+package org.arquillian.cube.kubernetes.api;
+
+import java.net.URL;
+
+public interface KubernetesResourceLocator extends WithToImmutable<KubernetesResourceLocator> {
+
+    /**
+     * Locates the kubernetes resource.
+     *
+     * @return Returns the url that points to the resource.
+     */
+    URL locate();
+}
---
+++ kubernetes/kubernetes/src/main/java/org/arquillian/cube/kubernetes/impl/locator/DefaultKubernetesResourceLocator.java
@@ -0,0 +1,41 @@
+package org.arquillian.cube.kubernetes.impl.locator;
+
+import java.net.URL;
+import org.arquillian.cube.kubernetes.api.KubernetesResourceLocator;
+
+public class DefaultKubernetesResourceLocator implements KubernetesResourceLocator {
+
+    private static final String ROOT = "/";
+    private static final String[] RESOURCE_NAMES = new String[] {"kubernetes", "META-INF/fabric8/kubernetes"};
+    private static final String[] ALLOWED_SUFFIXES = {".json", ".yml", ".yaml"};
+
+    @Override
+    public URL locate() {
+        for (String suffix : getAllowedSuffixes()) {
+            for (String resource : getResourceNames()) {
+                URL candidate = getResource(resource + suffix);
+                if (candidate != null) {
+                    return candidate;
+                }
+            }
+        }
+        return null;
+    }
+
+    protected String[] getResourceNames() {
+        return RESOURCE_NAMES;
+    }
+
+    protected String[] getAllowedSuffixes() {
+        return ALLOWED_SUFFIXES;
+    }
+
+    URL getResource(String resource) {
+        return KubernetesResourceLocator.class.getResource(resource.startsWith(ROOT) ? resource : ROOT + resource);
+    }
+
+    @Override
+    public KubernetesResourceLocator toImmutable() {
+        return this;
+    }
+}
