cb56206342d960261ae52013f5acacdceb9aacb4
repairnator <noreply@github.com>
2020-03-18 10:37:40 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-derjust-spring-data-dynamodb-352674421-20180313-055331-firstCommit

---
+++ src/main/java/org/socialsignin/spring/data/dynamodb/query/AbstractQuery.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright © 2018 spring-data-dynamodb (https://github.com/derjust/spring-data-dynamodb)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.socialsignin.spring.data.dynamodb.query;
+
+import org.socialsignin.spring.data.dynamodb.core.DynamoDBOperations;
+
+import java.util.Optional;
+
+/**
+ * {@link org.socialsignin.spring.data.dynamodb.mapping.DynamoDBPersistentProperty} implementation
+ *
+ * @author Michael Lavelle
+ * @author Sebastian Just
+ */
+public abstract class AbstractQuery<T> implements Query<T> {
+
+	protected final DynamoDBOperations dynamoDBOperations;
+	protected final Class<T> clazz;
+	protected boolean scanEnabled = false;
+	protected boolean scanCountEnabled = false;
+	protected Optional<String> projectionExpression = Optional.empty();
+
+
+	public boolean isScanCountEnabled() {
+		return this.scanCountEnabled;
+	}
+
+	public boolean isScanEnabled() {
+		return this.scanEnabled;
+	}
+
+	public Optional<String> getProjectionExpression() {
+		return this.projectionExpression;
+	}
+
+	@Override
+    public void setScanCountEnabled(boolean scanCountEnabled) {
+		this.scanCountEnabled = scanCountEnabled;
+	}
+
+	@Override
+    public void setScanEnabled(boolean scanEnabled) {
+		this.scanEnabled = scanEnabled;
+	}
+
+	@Override
+	public void setProjectionExpression(Optional<String> projectionExpression) {
+		this.projectionExpression = projectionExpression;
+	}
+
+	public AbstractQuery(DynamoDBOperations dynamoDBOperations, Class<T> clazz) {
+		this.dynamoDBOperations = dynamoDBOperations;
+		this.clazz = clazz;
+	}
+
+}
