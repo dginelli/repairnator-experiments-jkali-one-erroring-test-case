e150fecd6a2758c9a07d427315c4cc50c950dec3
repairnator <noreply@github.com>
2020-03-18 10:34:59 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-cyenyxe-eva-accession-390858054-20180611-182351-firstCommit

---
+++ eva-accession-ws/src/main/java/uk/ac/ebi/eva/accession/ws/rest/SubmittedVariantsRestController.java
@@ -0,0 +1,48 @@
+/*
+ *
+ * Copyright 2018 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package uk.ac.ebi.eva.accession.ws.rest;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.ac.ebi.ampt2d.commons.accession.rest.AccessionResponseDTO;
+import uk.ac.ebi.ampt2d.commons.accession.rest.BasicRestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/v1/submitted-variants")
+@Api(tags = {"Submitted variants"})
+public class SubmittedVariantsRestController {
+
+    private final BasicRestController basicRestController;
+
+    public SubmittedVariantsRestController(BasicRestController basicRestController) {
+        this.basicRestController = basicRestController;
+    }
+
+    @ApiOperation(value = "Find submitted variants by identifier")
+    @GetMapping(value = "/{identifiers}", produces = "application/json")
+    public List<AccessionResponseDTO> get(@PathVariable List<Long> identifiers) {
+        return basicRestController.get(identifiers);
+    }
+}
+
