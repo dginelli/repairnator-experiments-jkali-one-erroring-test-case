6c01f0cf996c4b61933889ac8871d10ee48e3b7d
repairnator <noreply@github.com>
2020-03-18 10:39:23 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-commercetools-commercetools-jvm-sdk-330394811-20180118-165144_bugonly-firstCommit

---
+++ commercetools-models/src/test/java/io/sphere/sdk/customergroups/CustomerGroupFixtures.java
@@ -0,0 +1,38 @@
+package io.sphere.sdk.customergroups;
+
+import io.sphere.sdk.client.BlockingSphereClient;
+import io.sphere.sdk.customergroups.commands.CustomerGroupCreateCommand;
+import io.sphere.sdk.customergroups.commands.CustomerGroupDeleteCommand;
+import io.sphere.sdk.customergroups.queries.CustomerGroupByIdGet;
+import io.sphere.sdk.customergroups.queries.CustomerGroupQuery;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+import static io.sphere.sdk.test.SphereTestUtils.*;
+
+public class CustomerGroupFixtures {
+    public static void withCustomerGroup(final BlockingSphereClient client, final Consumer<CustomerGroup> consumer) {
+        final String name = randomString();
+        final String key = randomKey();
+        final CustomerGroupDraft customerGroupDraft = CustomerGroupDraft.of(name,key);
+        final CustomerGroup customerGroup = client.executeBlocking(CustomerGroupCreateCommand.of(customerGroupDraft));
+        consumer.accept(customerGroup);
+        final Optional<CustomerGroup> customerGroupOptional = Optional.ofNullable(client.executeBlocking(CustomerGroupByIdGet.of(customerGroup.getId())));
+        customerGroupOptional.ifPresent(group -> client.executeBlocking(CustomerGroupDeleteCommand.of(group)));
+    }
+
+    public static void withB2cCustomerGroup(final BlockingSphereClient client, final Consumer<CustomerGroup> consumer) {
+        final CustomerGroup customerGroup = b2cCustomerGroup(client);
+        consumer.accept(customerGroup);
+    }
+
+    public static CustomerGroup b2cCustomerGroup(final BlockingSphereClient client) {
+        return client.executeBlocking(CustomerGroupQuery.of().byName("b2c")).head().orElseGet(() -> client.executeBlocking(CustomerGroupCreateCommand.of("b2c")));
+    }
+
+    public static void withCustomerGroup(final BlockingSphereClient client, final CustomerGroupDraft draft, final Consumer<CustomerGroup> consumer) {
+        final CustomerGroup customerGroup = client.executeBlocking(CustomerGroupCreateCommand.of(draft));
+        consumer.accept(customerGroup);
+        client.executeBlocking(CustomerGroupDeleteCommand.of(customerGroup));
+    }
+}
---
+++ commercetools-sdk-base/src/main/java/io/sphere/sdk/json/MoneyRepresentation.java
@@ -0,0 +1,62 @@
+package io.sphere.sdk.json;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.javamoney.moneta.function.MonetaryQueries;
+import org.javamoney.moneta.internal.DefaultRoundingProvider;
+
+import javax.money.Monetary;
+import javax.money.MonetaryAmount;
+import javax.money.MonetaryRounding;
+import javax.money.RoundingQueryBuilder;
+
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
+final class MoneyRepresentation {
+    private final Long centAmount;
+    private final String currencyCode;
+
+    private static final DefaultRoundingProvider ROUNDING_PROVIDER = new DefaultRoundingProvider();
+
+    @JsonCreator
+    private MoneyRepresentation(final Long centAmount, final String currencyCode) {
+        this.centAmount = centAmount;
+        this.currencyCode = currencyCode;
+    }
+
+    /**
+     * Creates a new Money instance.
+     * Money can't represent cent fractions. The value will be rounded to nearest cent value using RoundingMode.HALF_EVEN.
+     * @param monetaryAmount the amount with currency to transform
+     */
+    @JsonIgnore
+    public MoneyRepresentation(final MonetaryAmount monetaryAmount) {
+        this(amountToCents(monetaryAmount), requireValidCurrencyCode(monetaryAmount.getCurrency().getCurrencyCode()));
+    }
+
+    public long getCentAmount() {
+        return centAmount;
+    }
+
+    /**
+     * @return The ISO 4217 currency code, for example "EUR" or "USD".
+     */
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    private static String requireValidCurrencyCode(final String currencyCode) {
+        if (isEmpty(currencyCode))
+            throw new IllegalArgumentException("Money.currencyCode can't be empty.");
+        return currencyCode;
+    }
+
+    public static long amountToCents(final MonetaryAmount monetaryAmount) {
+        final MonetaryRounding ROUNDING =
+                ROUNDING_PROVIDER.getRounding(RoundingQueryBuilder.of().setRoundingName("default").setCurrency(monetaryAmount.getCurrency())
+                .build());
+        return monetaryAmount
+                .with(ROUNDING)
+                .query(MonetaryQueries.convertMinorPart());
+    }
+}
