5846d30a9d51f67a666f9ade48dc21a3da1175f5
repairnator <noreply@github.com>
2020-03-18 10:45:57 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-apache-incubator-dubbo-415750505-20180814-075834-firstCommit

---
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/protocol/AbstractProtocol.java
@@ -0,0 +1,120 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.dubbo.rpc.protocol;
+
+import org.apache.dubbo.common.Constants;
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.logger.Logger;
+import org.apache.dubbo.common.logger.LoggerFactory;
+import org.apache.dubbo.common.utils.ConcurrentHashSet;
+import org.apache.dubbo.remoting.RemotingException;
+import org.apache.dubbo.rpc.Exporter;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Protocol;
+import org.apache.dubbo.rpc.RpcException;
+import org.apache.dubbo.rpc.support.ProtocolUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * abstract ProtocolSupport.
+ */
+public abstract class AbstractProtocol implements Protocol {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    protected final Map<String, Exporter<?>> exporterMap = new ConcurrentHashMap<String, Exporter<?>>();
+
+    //TODO SOFEREFENCE
+    protected final Set<Invoker<?>> invokers = new ConcurrentHashSet<Invoker<?>>();
+
+    protected static String serviceKey(URL url) {
+        int port = url.getParameter(Constants.BIND_PORT_KEY, url.getPort());
+        return serviceKey(port, url.getPath(), url.getParameter(Constants.VERSION_KEY),
+                url.getParameter(Constants.GROUP_KEY));
+    }
+
+    protected static String serviceKey(int port, String serviceName, String serviceVersion, String serviceGroup) {
+        return ProtocolUtils.serviceKey(port, serviceName, serviceVersion, serviceGroup);
+    }
+    /*
+    protected Object tryConnect(URL url) throws RemotingException {
+        throw new RpcException("tryConnect is not override");
+    }
+    */
+
+    protected Object tryConnect(URL url) throws RemotingException{
+        throw new RpcException("tryConnect is not override");
+    }
+
+    protected Object doBind(URL url) throws RpcException{
+
+        while(true){
+            try {
+                return tryConnect(url);
+            } catch (RemotingException e) {
+                String randomPort = url.getParameter("randomPort");
+                if(randomPort != null && randomPort.equalsIgnoreCase("true")){
+                    int oldPort = url.getPort();
+                    URL oldUrl = url;
+                    Map<String, String> map = new HashMap(url.getParameters());
+                    map.put(Constants.BIND_PORT_KEY, String.valueOf(oldPort+1));
+                    url = new URL(oldUrl.getProtocol(),oldUrl.getUsername(),oldUrl.getPassword(),oldUrl.getHost(),
+                            oldPort+1,oldUrl.getPath(),map);
+                    url.setPort(oldPort+1);
+                    //url.getParameters().put(Constants.BIND_PORT_KEY, String.valueOf(oldPort+1));
+                }else{
+                    throw new RpcException("Fail to start server(url: " + url + ") " + e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+        for (Invoker<?> invoker : invokers) {
+            if (invoker != null) {
+                invokers.remove(invoker);
+                try {
+                    if (logger.isInfoEnabled()) {
+                        logger.info("Destroy reference: " + invoker.getUrl());
+                    }
+                    invoker.destroy();
+                } catch (Throwable t) {
+                    logger.warn(t.getMessage(), t);
+                }
+            }
+        }
+        for (String key : new ArrayList<String>(exporterMap.keySet())) {
+            Exporter<?> exporter = exporterMap.remove(key);
+            if (exporter != null) {
+                try {
+                    if (logger.isInfoEnabled()) {
+                        logger.info("Unexport service: " + exporter.getInvoker().getUrl());
+                    }
+                    exporter.unexport();
+                } catch (Throwable t) {
+                    logger.warn(t.getMessage(), t);
+                }
+            }
+        }
+    }
+}
