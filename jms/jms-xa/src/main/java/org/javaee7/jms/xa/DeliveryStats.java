a28a0b593be64f9fbcf42abb68ac90e7cab935b1
repairnator <noreply@github.com>
2020-03-19 11:19:25 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-javaee-samples-javaee7-samples-237205217-20170530-030228_bugonly-firstCommit

---
+++ jms/jms-xa/src/main/java/org/javaee7/jms/xa/DeliveryStats.java
@@ -0,0 +1,21 @@
+package org.javaee7.jms.xa;
+
+import javax.ejb.Singleton;
+
+@Singleton
+public class DeliveryStats {
+
+    private long deliveredMessagesCount;
+
+    public long getDeliveredMessagesCount() {
+        return deliveredMessagesCount;
+    }
+
+    public void messageDelivered() {
+        deliveredMessagesCount++;
+    }
+
+    public void reset() {
+        deliveredMessagesCount = 0L;
+    }
+}
