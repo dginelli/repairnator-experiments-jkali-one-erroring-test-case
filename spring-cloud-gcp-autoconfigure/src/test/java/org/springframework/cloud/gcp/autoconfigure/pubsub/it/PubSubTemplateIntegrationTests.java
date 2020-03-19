f29108a6328eb62497b98955c7922949e0cf2fa1
repairnator <noreply@github.com>
2020-03-19 19:50:28 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-spring-cloud-spring-cloud-gcp-395219371-20180621-232656-firstCommit

---
+++ spring-cloud-gcp-autoconfigure/src/test/java/org/springframework/cloud/gcp/autoconfigure/pubsub/it/PubSubTemplateIntegrationTests.java
@@ -0,0 +1,143 @@
+/*
+ *  Copyright 2018 original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.springframework.cloud.gcp.autoconfigure.pubsub.it;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.PubsubMessage;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.cloud.gcp.autoconfigure.core.GcpContextAutoConfiguration;
+import org.springframework.cloud.gcp.autoconfigure.pubsub.GcpPubSubAutoConfiguration;
+import org.springframework.cloud.gcp.pubsub.PubSubAdmin;
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
+import org.springframework.cloud.gcp.pubsub.support.AcknowledgeablePubsubMessage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assumptions.assumeThat;
+
+/**
+ * @author João André Martins
+ * @author Chengyuan Zhao
+ */
+public class PubSubTemplateIntegrationTests {
+
+	private ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+			.withConfiguration(AutoConfigurations.of(GcpContextAutoConfiguration.class,
+					GcpPubSubAutoConfiguration.class));
+
+	@BeforeClass
+	public static void enableTests() {
+			assumeThat(System.getProperty("it.pubsub")).isEqualTo("true");
+	}
+
+	@Test
+	public void testCreatePublishPullNextAndDelete() {
+		this.contextRunner.run(context -> {
+			PubSubAdmin pubSubAdmin = context.getBean(PubSubAdmin.class);
+			PubSubTemplate pubSubTemplate = context.getBean(PubSubTemplate.class);
+
+			String topicName = "tarkus_" + UUID.randomUUID();
+			String subscriptionName = "zatoichi_" + UUID.randomUUID();
+
+			assertThat(pubSubAdmin.getTopic(topicName)).isNull();
+			assertThat(pubSubAdmin.getSubscription(subscriptionName))
+					.isNull();
+			pubSubAdmin.createTopic(topicName);
+			pubSubAdmin.createSubscription(subscriptionName, topicName);
+
+			Map<String, String> headers = new HashMap<>();
+			headers.put("cactuar", "tonberry");
+			headers.put("fujin", "raijin");
+			pubSubTemplate.publish(topicName, "tatatatata", headers);
+			PubsubMessage pubsubMessage = pubSubTemplate.pullNext(subscriptionName);
+
+			assertThat(pubsubMessage.getData()).isEqualTo(ByteString.copyFromUtf8("tatatatata"));
+			assertThat(pubsubMessage.getAttributesCount()).isEqualTo(2);
+			assertThat(pubsubMessage.getAttributesOrThrow("cactuar")).isEqualTo("tonberry");
+			assertThat(pubsubMessage.getAttributesOrThrow("fujin")).isEqualTo("raijin");
+
+			assertThat(pubSubAdmin.getTopic(topicName)).isNotNull();
+			assertThat(pubSubAdmin.getSubscription(subscriptionName)).isNotNull();
+			assertThat(pubSubAdmin.listTopics().stream()
+					.filter(topic -> topic.getName().endsWith(topicName)).toArray().length)
+							.isEqualTo(1);
+			assertThat(pubSubAdmin.listSubscriptions().stream().filter(
+					subscription -> subscription.getName().endsWith(subscriptionName))
+					.toArray().length).isEqualTo(1);
+			pubSubAdmin.deleteSubscription(subscriptionName);
+			pubSubAdmin.deleteTopic(topicName);
+			assertThat(pubSubAdmin.getTopic(topicName)).isNull();
+			assertThat(pubSubAdmin.getSubscription(subscriptionName)).isNull();
+			assertThat(pubSubAdmin.listTopics().stream()
+					.filter(topic -> topic.getName().endsWith(topicName)).toArray().length)
+					.isEqualTo(0);
+			assertThat(pubSubAdmin.listSubscriptions().stream().filter(
+					subscription -> subscription.getName().endsWith(subscriptionName))
+					.toArray().length).isEqualTo(0);
+		});
+	}
+
+	@Test
+	public void testPullAndAck() {
+		this.contextRunner.run(context -> {
+			PubSubAdmin pubSubAdmin = context.getBean(PubSubAdmin.class);
+			String topicName = "peel-the-paint" + UUID.randomUUID();
+			String subscriptionName = "i-lost-my-head" + UUID.randomUUID();
+			pubSubAdmin.createTopic(topicName);
+			pubSubAdmin.createSubscription(subscriptionName, topicName);
+
+			PubSubTemplate pubSubTemplate = context.getBean(PubSubTemplate.class);
+
+			pubSubTemplate.publish(topicName, "free-hand");
+			pubSubTemplate.publish(topicName, "valedictory");
+			pubSubTemplate.publish(topicName, "the-runaway");
+
+			List<AcknowledgeablePubsubMessage> ackableMessages =
+					pubSubTemplate.pull(subscriptionName, 4, true);
+
+			assertThat(ackableMessages.size()).isEqualTo(3);
+
+			ackableMessages.forEach(message -> {
+				if (message.getMessage().getData().toStringUtf8().equals("free-hand")) {
+					message.ack();
+				}
+				else {
+					message.nack();
+				}
+			});
+
+			ackableMessages = pubSubTemplate.pull(subscriptionName, 4, true);
+
+			assertThat(ackableMessages.size()).isEqualTo(2);
+			ackableMessages.forEach(AcknowledgeablePubsubMessage::ack);
+
+			ackableMessages = pubSubTemplate.pull(subscriptionName, 4, true);
+			assertThat(ackableMessages.size()).isEqualTo(0);
+
+			pubSubAdmin.deleteSubscription(subscriptionName);
+			pubSubAdmin.deleteTopic(topicName);
+		});
+	}
+}
---
+++ spring-cloud-gcp-pubsub/src/main/java/org/springframework/cloud/gcp/pubsub/integration/inbound/PubSubInboundChannelAdapter.java
@@ -0,0 +1,140 @@
+/*
+ *  Copyright 2017-2018 original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.springframework.cloud.gcp.pubsub.integration.inbound;
+
+import java.util.Map;
+
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+import com.google.cloud.pubsub.v1.Subscriber;
+import com.google.pubsub.v1.PubsubMessage;
+
+import org.springframework.cloud.gcp.pubsub.core.PubSubException;
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
+import org.springframework.cloud.gcp.pubsub.integration.AckMode;
+import org.springframework.cloud.gcp.pubsub.integration.PubSubHeaderMapper;
+import org.springframework.cloud.gcp.pubsub.support.GcpPubSubHeaders;
+import org.springframework.integration.endpoint.MessageProducerSupport;
+import org.springframework.integration.mapping.HeaderMapper;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.util.Assert;
+
+/**
+ * Converts from GCP Pub/Sub message to Spring message and sends the Spring message to the
+ * attached channels.
+ *
+ * @author João André Martins
+ * @author Mike Eltsufin
+ */
+public class PubSubInboundChannelAdapter extends MessageProducerSupport {
+
+	private final String subscriptionName;
+
+	private final PubSubTemplate pubSubTemplate;
+
+	private Subscriber subscriber;
+
+	private AckMode ackMode = AckMode.AUTO;
+
+	private HeaderMapper<Map<String, String>> headerMapper = new PubSubHeaderMapper();
+
+	private Class payloadType = byte[].class;
+
+	public PubSubInboundChannelAdapter(PubSubTemplate pubSubTemplate, String subscriptionName) {
+		this.pubSubTemplate = pubSubTemplate;
+		this.subscriptionName = subscriptionName;
+	}
+
+	@Override
+	protected void doStart() {
+		super.doStart();
+
+		this.subscriber =
+				this.pubSubTemplate.subscribe(this.subscriptionName, this::receiveMessage);
+	}
+
+	private void receiveMessage(PubsubMessage pubsubMessage, AckReplyConsumer consumer) {
+		Map<String, Object> messageHeaders =
+				this.headerMapper.toHeaders(pubsubMessage.getAttributesMap());
+
+		if (this.ackMode == AckMode.MANUAL) {
+			// Send the consumer downstream so user decides on when to ack/nack.
+			messageHeaders.put(GcpPubSubHeaders.ACKNOWLEDGEMENT, consumer);
+		}
+
+		try {
+			sendMessage(MessageBuilder.withPayload(
+					this.pubSubTemplate.getMessageConverter().fromPubSubMessage(pubsubMessage, this.payloadType))
+					.copyHeaders(messageHeaders)
+					.build());
+		}
+		catch (RuntimeException re) {
+			if (this.ackMode == AckMode.AUTO) {
+				consumer.nack();
+			}
+			throw new PubSubException("Sending Spring message failed.", re);
+		}
+
+		if (this.ackMode == AckMode.AUTO) {
+			consumer.ack();
+		}
+	}
+
+	@Override
+	protected void doStop() {
+		if (this.subscriber != null) {
+			this.subscriber.stopAsync();
+		}
+
+		super.doStop();
+	}
+
+	public AckMode getAckMode() {
+		return this.ackMode;
+	}
+
+	public void setAckMode(AckMode ackMode) {
+		Assert.notNull(ackMode, "The acknowledgement mode can't be null.");
+		this.ackMode = ackMode;
+	}
+
+	public Class getPayloadType() {
+		return this.payloadType;
+	}
+
+	/**
+	 * Set the desired type of the payload of the {@link org.springframework.messaging.Message} constructed by
+	 * converting the incoming Pub/Sub message. The channel adapter will use the
+	 * {@link PubSubTemplate#getMessageConverter()} to do the conversion to the desired type.
+	 * The default payload type is {@code byte[].class}
+	 * @param payloadType the type of the payload of the {@link org.springframework.messaging.Message} produce by the
+	 * 				adapter. Cannot be set to null.
+	 */
+	public void setPayloadType(Class payloadType) {
+		Assert.notNull(payloadType, "The payload type cannot be null.");
+		this.payloadType = payloadType;
+	}
+
+	/**
+	 * Set the header mapper to map headers from incoming {@link PubsubMessage} into
+	 * {@link org.springframework.messaging.Message}.
+	 * @param headerMapper the header mapper
+	 */
+	public void setHeaderMapper(HeaderMapper<Map<String, String>> headerMapper) {
+		Assert.notNull(headerMapper, "The header mapper can't be null.");
+		this.headerMapper = headerMapper;
+	}
+}
