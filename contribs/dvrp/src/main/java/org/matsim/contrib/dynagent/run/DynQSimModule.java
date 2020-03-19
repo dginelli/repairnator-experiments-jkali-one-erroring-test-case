583c26985747f92d2a542f7814d88e77103e9224
repairnator <noreply@github.com>
2020-03-19 11:28:36 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-matsim-org-matsim-411884142-20180803-215451-firstCommit

---
+++ contribs/dvrp/src/main/java/org/matsim/contrib/dynagent/run/DynQSimModule.java
@@ -0,0 +1,71 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2016 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.contrib.dynagent.run;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+import org.matsim.core.config.Config;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.mobsim.framework.AgentSource;
+import org.matsim.core.mobsim.qsim.AbstractQSimPlugin;
+import org.matsim.core.mobsim.qsim.ActivityEnginePlugin;
+import org.matsim.core.mobsim.qsim.QSimModule;
+import org.matsim.core.mobsim.qsim.components.QSimComponents;
+import org.matsim.core.mobsim.qsim.components.StandardQSimComponentsConfigurator;
+
+import com.google.inject.Provides;
+
+public class DynQSimModule extends AbstractModule {
+	private final Class<? extends AgentSource> agentSourceClass;
+
+	public DynQSimModule(Class<? extends AgentSource> agentSourceClass) {
+		this.agentSourceClass = agentSourceClass;
+	}
+
+	@Override
+	public void install() {
+	}
+
+	@Provides
+	public Collection<AbstractQSimPlugin> provideDynQSimPlugins(Config config) {
+		return provideQSimPlugins(config, agentSourceClass);
+	}
+
+	public static Collection<AbstractQSimPlugin> provideQSimPlugins(Config config,
+			Class<? extends AgentSource> agentSourceClass) {
+		//use the standard plugins, but replace ActivityEnginePlugin with DynActivityEnginePlugin
+		Collection<AbstractQSimPlugin> plugins = QSimModule.getDefaultQSimPlugins(config)
+				.stream()
+				.map(p -> p instanceof ActivityEnginePlugin ? new DynActivityEnginePlugin(config) : p)
+				.collect(Collectors.toList());
+		plugins.add(new DynAgentSourcePlugin(config, agentSourceClass));
+		return Collections.unmodifiableCollection(plugins);
+	}
+
+	@Provides
+	private QSimComponents provideQSimComponents(Config config) {
+		QSimComponents components = new QSimComponents();
+		new StandardQSimComponentsConfigurator(config).configure(components);
+		new DynQSimComponentsConfigurator().configure(components);
+		return components;
+	}
+}
