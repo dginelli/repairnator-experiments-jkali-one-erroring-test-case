4a9ecf9a8d4af7ad7d902a77670bb3c8b9a7db31
repairnator <noreply@github.com>
2020-03-19 10:59:33 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-jamersonapce-projeto01-topicos2-373733270-20180502-034856-firstCommit

---
+++ src/main/java/com/jamerson/topicos2/primeiroprojeto/PrimeiroProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.jamerson.topicos2.primeiroprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeiroProjetoApplication.class, args);
+        System.out.println("Meu primeiro projeto com spring boot ....");
+    }
+}
