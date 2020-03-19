2b9c5e2fa42465da1a015f7101b435f916bb0e86
repairnator <noreply@github.com>
2020-03-19 10:59:33 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-it-kmitl-2018-group7-375464036-20180506-092505-firstCommit

---
+++ src/main/java/th/ac/kmitl/it/soa/group7/TaxInvoiceApplication.java
@@ -0,0 +1,12 @@
+package th.ac.kmitl.it.soa.group7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxInvoiceApplication.class, args);
+	}
+}
