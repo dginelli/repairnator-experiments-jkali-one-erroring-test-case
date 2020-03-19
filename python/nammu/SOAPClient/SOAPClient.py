47f2c818ed50c8b309cad9d50cca3108ecab85a2
repairnator <noreply@github.com>
2020-03-19 11:11:51 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-oracc-nammu-411794859-20180803-174042-firstCommit

---
+++ python/nammu/SOAPClient/SOAPClient.py
@@ -0,0 +1,153 @@
+'''
+Copyright 2015 - 2017 University College London.
+
+This file is part of Nammu.
+
+Nammu is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Nammu is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Nammu.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
+import StringIO
+import re
+import os
+import logging
+import logging.config
+import requests
+import urllib
+from java.lang import System, ClassLoader
+from zipfile import ZipFile
+from logging import Formatter
+from requests.exceptions import RequestException, ConnectTimeout
+from HTTPRequest import HTTPRequest
+import xml.etree.ElementTree as ET
+
+
+class SOAPClient(object):
+    """
+    Sends and retrieves information to and from the ORACC SOAP server.
+    """
+    def __init__(self, url, port, url_dir, method):
+        self.url = url
+        self.port = port
+        self.url_dir = url_dir
+        self.method = method
+        # TODO: Create logger in this module that reuses nammu controller's
+        # logger configuration so output is in same file, but tells us it was
+        # produced in this module.
+        self.logger, self.request_log = self.setup_logger()
+
+    def create_request(self, **kwargs):
+        url = "{}:{}".format(self.url, self.port)
+        request = HTTPRequest(url, self.method, **kwargs)
+        self.request = request
+
+    def send(self):
+        """
+        Elaborate HTTP POST request and send it to ORACC's server.
+        """
+        url = "{}:{}".format(self.url, self.port)
+        headers = dict(self.request.get_headers())
+        body = self.request.get_body()
+        self.logger.debug("Sending request to server at %s.", url)
+        self.logger.debug("HTTP request headers sent: %s", headers)
+        self.logger.debug("HTTP request body sent: %s", body)
+        try:
+            self.response = requests.post(url, data=body, headers=headers,
+                                          timeout=3)
+        except ConnectTimeout:
+            self.logger.error('Connection timed out when sending POST '
+                              'request.')
+            raise
+
+    def get_response_text(self):
+        return self.response.text
+
+    def get_response_id(self):
+        xml_root = ET.fromstring(self.response.text)
+        # This should be done with xpath. See XPath and namespaces sections
+        # here: https://docs.python.org/2/library/xml.etree.elementtree.html
+        return xml_root[0][0][0][0].text
+
+    def wait_for_response(self, request_id):
+        """
+        Check for a response to the request and obtain response zip file.
+        Since it's an asynchronous communication, when sending a request for
+        validation or lemmatisation, the server returns an ID for us to ask
+        via HTTP GET until the request is ready.
+        The response from the server can be:
+        * "run\n" (request is being processed)
+        * "done\n" (request is ready - we can send a new SOAP request to get
+                    it)
+        *  "err_stat\n" (something bad happened and we have to mail Steve)
+        """
+        url = "{}/{}/{}".format(self.url, self.url_dir, request_id)
+        while True:
+            try:
+                response = requests.get(url)
+            except RequestException:
+                raise
+            else:
+                if response.text == "done\n":
+                    return
+                elif response.text == "err_stat\n":
+                    raise Exception("UnknownServerError")
+
+    def get_response(self):
+        return self.response.content
+
+    def get_server_logs(self):
+        """
+        Manipulate response to substract the content of oracc.log that is in
+        the returned binary-coded zip file.
+        """
+        self.response.content
+        binary_body = re.split('--==.*==',
+                               self.response.content)[2].split('\r\n')[5]
+
+        f = StringIO.StringIO()
+        f.write(bytearray(binary_body))
+
+        memory_zip = ZipFile(f)
+        namelist = memory_zip.namelist()
+        zip_content = {name: memory_zip.read(name) for name in namelist}
+        oracc_log = zip_content['oracc.log']
+        request_log = zip_content['request.log']
+
+        # Check if server returns a lemmatised file
+        autolem = None
+        for key, value in zip_content.iteritems():
+            if key.endswith("autolem.atf"):
+                autolem = value
+
+        self.logger.debug("The returned file from server contains: %s",
+                          zip_content.keys())
+
+        for file in zip_content.keys():
+            self.logger.debug("These are the contents of %s: \n%s",
+                              file,
+                              zip_content[file])
+
+        return oracc_log, request_log, autolem
+
+    def setup_logger(self):
+        """
+        Creates logger for Nammu's functionality as well as to debug HTTP
+        messages sent to the ORACC server and responses received.
+        Output should be sent to Nammu's console as well as a local logfile and
+        the system console.
+        """
+        logger = logging.getLogger("SOAPClient")
+        requests_logger = logging.getLogger("requests.packages.urllib3")
+
+        return logger, requests_logger
---
+++ python/nammu/view/ModelView.py
@@ -0,0 +1,171 @@
+'''
+Copyright 2015 - 2017 University College London.
+
+This file is part of Nammu.
+
+Nammu is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Nammu is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Nammu.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from java.awt import BorderLayout, GridLayout, Color, Dimension
+from javax.swing import JScrollPane, JPanel, JFrame, JComboBox, JTabbedPane
+from javax.swing import JLabel, BoxLayout, JButton
+
+
+class ModelView(JFrame):
+    '''
+    Initializes the ATF model view and sets its layout.
+    '''
+    def __init__(self, controller):
+        '''
+        Creates a new window displaying a schematic view of an ATF file,
+        following the guidelines and mockup agreed with project owners.
+        '''
+
+        # Give reference to controller to delegate action response
+        self.controller = controller
+
+        # Get list of projects, languages and protocols from config settings
+        self.languages = self.controller.config['languages']
+        self.protocols = self.controller.config['protocols']
+        self.projects = self.controller.config['projects']
+
+        # Make text area occupy all available space and resize with parent
+        # window
+        self.setLayout(BorderLayout())
+
+        self.mainPanel = JTabbedPane()
+        self.mainPanel.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT)
+        self.add(self.mainPanel, BorderLayout.CENTER)
+
+        # Set empty dictionary of tab panels
+        self.objectTabs = {}
+
+        # Will need scrolling controls
+        scrollingArea = JScrollPane(self.mainPanel)
+
+        # Add notice panel
+        self.add(self.addNotice(), BorderLayout.NORTH)
+
+        # Add to parent panel
+        self.add(scrollingArea, BorderLayout.CENTER)
+
+    def addObject(self, objectID):
+        """
+        Creates new empty JPanel that'll contain the model for one object in
+        the ATF file.
+        """
+        objectPanel = JPanel()
+        objectPanel.setLayout(BoxLayout(objectPanel, BoxLayout.PAGE_AXIS))
+
+        self.objectTabs[objectID] = objectPanel
+
+    def display(self):
+        """
+        Put together all elements in main panel and display.
+        """
+        # Add all object tabs to window
+        for objectID, tabPanel in self.objectTabs.iteritems():
+            self.mainPanel.add(objectID, tabPanel)
+
+        # Set up main model window
+        self.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE)
+        self.setTitle("ATF Model View")
+        self.pack()
+        self.setLocationRelativeTo(None)
+
+        # Display model window
+        self.visible = 1
+
+    def addLine(self, objectID, category, text):
+        """
+        Add a new panel containing the text corresponding to one line in the
+        ATF file.
+        This panel will show the line type (ruling, comment, text,
+        translation...), followed by the line content and a group of icons to
+        add, edit or remove the line.
+        """
+        linePanel = JPanel()
+        linePanel.setLayout(BorderLayout())
+
+        label = JLabel(category)
+
+        combo = JComboBox(text)
+        combo.setEditable(True)
+        combo.setPreferredSize(Dimension(500, 20))
+        combo.setSize(combo.getPreferredSize())
+        combo.setMinimumSize(combo.getPreferredSize())
+        combo.setMaximumSize(combo.getPreferredSize())
+
+        buttonsPanel = JPanel()
+        addButton = JButton("Add")
+        editButton = JButton("Edit")
+        deleteButton = JButton("Delete")
+        buttonsPanel.add(addButton)
+        buttonsPanel.add(editButton)
+        buttonsPanel.add(deleteButton)
+
+        linePanel.add(label, BorderLayout.WEST)
+        linePanel.add(combo, BorderLayout.CENTER)
+        linePanel.add(buttonsPanel, BorderLayout.EAST)
+
+        # Add metadataPanel to object tab in main panel
+        self.objectTabs[objectID].add(linePanel)
+
+    # Protocols not yet in model parsed object
+    # def addMetadata(self, project, language, protocols):
+    def addMetadata(self, objectID, project, language):
+        """
+        Add a JTable at the top of the object tab containing the metadata of
+        the object presented in that tab.
+        """
+        metadataPanel = JPanel()
+        # TODO: Need to count protocols to set up Grid dimension
+        metadataPanel.setLayout(GridLayout(3, 2))
+
+        projectLabel = JLabel("Project: ")
+        projectValue = JLabel(project)
+
+        languageLabel = JLabel("Language: ")
+        languageValue = JLabel(language)
+        # If language code is in the settings, then display name instead
+        # of code
+        for lang, code in self.languages.iteritems():
+            if code == language:
+                languageValue.setText(lang)
+
+        # TODO Protocols not yet in parsed object
+        protocolsLabel = JLabel("ATF Protocols: ")
+        protocolsBox = JComboBox(self.protocols)
+
+        metadataPanel.add(projectLabel)
+        metadataPanel.add(projectValue)
+        metadataPanel.add(languageLabel)
+        metadataPanel.add(languageValue)
+        metadataPanel.add(protocolsLabel)
+        metadataPanel.add(protocolsBox)
+
+        # Add metadataPanel to object tab in main panel
+        self.objectTabs[objectID].add(metadataPanel)
+
+    def addNotice(self):
+        """
+        Add a panel that notifies the user about the model view not being
+        ready yet.
+        """
+        panel = JPanel()
+        panel.setBackground(Color.yellow)
+        label = JLabel("Please note Nammu's model view is under "
+                       "construction.")
+        panel.add(label)
+        return panel
