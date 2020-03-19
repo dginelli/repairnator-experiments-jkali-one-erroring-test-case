a0d7b24219953ef384dc2c0bdd34558f0d6809c6
repairnator <noreply@github.com>
2020-03-19 19:48:06 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-runelite-runelite-375125632-20180505-013717-firstCommit

---
+++ runelite-client/src/main/java/net/runelite/client/config/RuneLiteConfig.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2017, Tyler <https://github.com/tylerthardy>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.config;
+
+import java.awt.Dimension;
+import net.runelite.api.Constants;
+import net.runelite.client.Notifier;
+
+@ConfigGroup(
+	keyName = "runelite",
+	name = "RuneLite",
+	description = "Configuration for RuneLite client options"
+)
+public interface RuneLiteConfig extends Config
+{
+	@ConfigItem(
+		keyName = "gameSize",
+		name = "Game size",
+		description = "The game will resize to this resolution upon starting the client",
+		position = 1
+	)
+	default Dimension gameSize()
+	{
+		return Constants.GAME_FIXED_SIZE;
+	}
+
+	@ConfigItem(
+		keyName = "lockWindowSize",
+		name = "Lock window size",
+		description = "Determines if the window resizing is allowed or not",
+		position = 2
+	)
+	default boolean lockWindowSize()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = "uiEnableCustomChrome",
+		name = "Enable custom window chrome",
+		description = "Use Runelite's custom window title and borders.",
+		warning = "Please restart your client after changing this setting",
+		position = 3
+	)
+	default boolean enableCustomChrome()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "gameAlwaysOnTop",
+		name = "Enable client always on top",
+		description = "The game will always be on the top of the screen",
+		position = 4
+	)
+	default boolean gameAlwaysOnTop()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = "notificationMode",
+		name = "Notification mode",
+		description = "Determines mode of notifications",
+		position = 5
+	)
+	default Notifier.NotificationMode notificationMode()
+	{
+		return Notifier.NotificationMode.TRAY;
+	}
+
+	@ConfigItem(
+		keyName = "notificationFocused",
+		name = "Send notifications when focused",
+		description = "Toggles idle notifications for when the client is focused",
+		position = 6
+	)
+	default boolean sendNotificationsWhenFocused()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = "notificationRequestFocus",
+		name = "Request focus on notification",
+		description = "Toggles window focus request",
+		position = 7
+	)
+	default boolean requestFocusOnNotification()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "fontType",
+		name = "Dynamic Overlay Font",
+		description = "Configures what font type is used for in-game overlays such as player name, ground items, etc.",
+		position = 8
+	)
+	default FontType fontType()
+	{
+		return FontType.SMALL;
+	}
+
+	@ConfigItem(
+		keyName = "infoBoxVertical",
+		name = "Display infoboxes vertically",
+		description = "Toggles the infoboxes to display vertically",
+		position = 9
+	)
+	default boolean infoBoxVertical()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = "infoBoxWrap",
+		name = "Infobox wrap count",
+		description = "Configures the amount of infoboxes shown before wrapping",
+		position = 10
+	)
+	default int infoBoxWrap()
+	{
+		return 4;
+	}
+
+	@ConfigItem(
+		keyName = "containInScreen",
+		name = "Contain in screen",
+		description = "Makes the client stay contained in the screen when attempted to move out of it.<br>Note: Only works if custom chrome is enabled.",
+		position = 11
+	)
+	default boolean containInScreen()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = "rememberScreenBounds",
+		name = "Remember client position",
+		description = "Save the position and size of the client after exiting",
+		position = 12
+	)
+	default boolean rememberScreenBounds()
+	{
+		return true;
+	}
+}
