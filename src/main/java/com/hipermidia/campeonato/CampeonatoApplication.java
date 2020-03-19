889616e349ccacd58baae6afe65df067aed3ff07
repairnator <noreply@github.com>
2020-03-19 19:41:59 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-wennersgc-API-campeonato-de-futebol-381803475-20180521-202748-firstCommit

---
+++ src/main/java/com/hipermidia/campeonato/CampeonatoApplication.java
@@ -0,0 +1,12 @@
+package com.hipermidia.campeonato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampeonatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampeonatoApplication.class, args);
+    }
+}
