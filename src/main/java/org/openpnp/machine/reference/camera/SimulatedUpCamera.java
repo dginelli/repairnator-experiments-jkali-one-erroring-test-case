d082c14e46592e11db7a4ab2321137ded5c376c9
repairnator <noreply@github.com>
2020-03-19 11:11:03 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-openpnp-openpnp-248822216-20170630-170810_bugonly-firstCommit

---
+++ src/main/java/org/openpnp/machine/reference/camera/SimulatedUpCamera.java
@@ -0,0 +1,203 @@
+package org.openpnp.machine.reference.camera;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+
+import org.openpnp.CameraListener;
+import org.openpnp.gui.support.Wizard;
+import org.openpnp.machine.reference.ReferenceCamera;
+import org.openpnp.machine.reference.wizards.ReferenceCameraConfigurationWizard;
+import org.openpnp.model.Configuration;
+import org.openpnp.model.Footprint;
+import org.openpnp.model.LengthUnit;
+import org.openpnp.model.Location;
+import org.openpnp.model.Part;
+import org.openpnp.spi.Head;
+import org.openpnp.spi.Nozzle;
+import org.openpnp.spi.PropertySheetHolder;
+import org.simpleframework.xml.Root;
+
+
+@Root
+public class SimulatedUpCamera extends ReferenceCamera implements Runnable {
+    protected int width = 640;
+
+    protected int height = 480;
+
+    protected int fps = 10;
+
+    private Thread thread;
+
+    public SimulatedUpCamera() {
+        setUnitsPerPixel(new Location(LengthUnit.Millimeters, 0.0234375D, 0.0234375D, 0, 0));
+        setLooking(Looking.Up);
+    }
+
+    @Override
+    public BufferedImage internalCapture() {
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = (Graphics2D) image.getGraphics();
+        AffineTransform tx = g.getTransform();
+        // invert the image in Y so that Y+ is up
+        g.translate(0, height);
+        g.scale(1, -1);
+        g.translate(width / 2, height / 2);
+
+        g.setColor(Color.black);
+        g.fillRect(0, 0, width, height);
+
+        // figure out our physical viewport size
+        Location phySize = getUnitsPerPixel().convertToUnits(LengthUnit.Millimeters)
+                                             .multiply(width, height, 0, 0);
+        double phyWidth = phySize.getX();
+        double phyHeight = phySize.getY();
+
+        // and bounds
+        Location location = getLocation().convertToUnits(LengthUnit.Millimeters);
+        Rectangle2D.Double phyBounds = new Rectangle2D.Double(location.getX() - phyWidth / 2,
+                location.getY() - phyHeight / 2, phyWidth, phyHeight);
+
+        // determine if there are any nozzles within our bounds and if so render them
+        for (Head head : Configuration.get()
+                                      .getMachine()
+                                      .getHeads()) {
+            for (Nozzle nozzle : head.getNozzles()) {
+                Location l = nozzle.getLocation()
+                                   .convertToUnits(LengthUnit.Millimeters);
+                if (phyBounds.contains(l.getX(), l.getY())) {
+                    drawNozzle(g, nozzle);
+                }
+            }
+        }
+
+        g.setTransform(tx);
+        g.dispose();
+        return image;
+    }
+
+
+    private void drawNozzle(Graphics2D g, Nozzle nozzle) {
+        g.setStroke(new BasicStroke(2f));
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        LengthUnit units = LengthUnit.Millimeters;
+        Location unitsPerPixel = getUnitsPerPixel().convertToUnits(units);
+        
+        // Draw the nozzle
+        // Get nozzle offsets from camera
+        Location offsets = nozzle.getLocation()
+                .convertToUnits(units)
+                .subtractWithRotation(getLocation());
+        
+        // Create a nozzle shape
+        fillShape(g, new Ellipse2D.Double(-0.5, -0.5, 1, 1), Color.green, unitsPerPixel, offsets);
+
+
+        // Draw the part
+        Part part = nozzle.getPart();
+        if (part == null) {
+            return;
+        }
+
+        org.openpnp.model.Package pkg = part.getPackage();
+        Footprint footprint = pkg.getFootprint();
+        if (footprint == null) {
+            return;
+        }
+
+        if (footprint.getUnits() != units) {
+            throw new Error("Not yet supported.");
+        }
+        
+        // First draw the body in dark grey.
+        fillShape(g, footprint.getBodyShape(), new Color(60, 60, 60), unitsPerPixel, offsets);
+        
+        // Then draw the pads in white
+        fillShape(g, footprint.getPadsShape(), Color.white, unitsPerPixel, offsets);
+    }
+    
+    private void fillShape(Graphics2D g, Shape shape, Color color, Location unitsPerPixel, Location offsets) {
+        AffineTransform tx = new AffineTransform();
+        // Scale to pixels
+        tx.scale(1.0 / unitsPerPixel.getX(), 1.0 / unitsPerPixel.getY());
+        // Translate and rotate to offsets
+        tx.translate(offsets.getX(), offsets.getY());
+        tx.rotate(Math.toRadians(offsets.getRotation()));
+        // Transform
+        shape = tx.createTransformedShape(shape);
+        // Draw
+        g.setColor(color);
+        g.fill(shape);
+    }
+    
+    @Override
+    public synchronized void startContinuousCapture(CameraListener listener, int maximumFps) {
+        start();
+        super.startContinuousCapture(listener, maximumFps);
+    }
+
+    @Override
+    public synchronized void stopContinuousCapture(CameraListener listener) {
+        super.stopContinuousCapture(listener);
+        if (listeners.size() == 0) {
+            stop();
+        }
+    }
+
+    private synchronized void stop() {
+        if (thread != null && thread.isAlive()) {
+            thread.interrupt();
+            try {
+                thread.join(3000);
+            }
+            catch (Exception e) {
+
+            }
+            thread = null;
+        }
+    }
+
+    private synchronized void start() {
+        if (thread == null) {
+            thread = new Thread(this);
+            thread.setDaemon(true);
+            thread.start();
+        }
+    }
+
+    public void run() {
+        while (!Thread.interrupted()) {
+            BufferedImage frame = internalCapture();
+            broadcastCapture(frame);
+            try {
+                Thread.sleep(1000 / fps);
+            }
+            catch (InterruptedException e) {
+                return;
+            }
+        }
+    }
+
+    @Override
+    public Wizard getConfigurationWizard() {
+        return new ReferenceCameraConfigurationWizard(this);
+    }
+
+    @Override
+    public String getPropertySheetHolderTitle() {
+        return getClass().getSimpleName() + " " + getName();
+    }
+
+    @Override
+    public PropertySheetHolder[] getChildPropertySheetHolders() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
