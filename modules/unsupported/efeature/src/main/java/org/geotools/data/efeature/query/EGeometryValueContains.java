25d8018472fde68e9d2cb4b2aa764c91fba24ef5
repairnator <noreply@github.com>
2020-03-19 11:19:21 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-geotools-geotools-376930944-20180509-193032-firstCommit

---
+++ modules/unsupported/efeature/src/main/java/org/geotools/data/efeature/query/EGeometryValueContains.java
@@ -0,0 +1,34 @@
+package org.geotools.data.efeature.query;
+
+import com.vividsolutions.jts.geom.Geometry;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.query.conditions.eobjects.structuralfeatures.EObjectAttributeValueCondition;
+import org.opengis.filter.expression.Literal;
+
+/** @source $URL$ */
+public class EGeometryValueContains extends EObjectAttributeValueCondition {
+
+    public EGeometryValueContains(EAttribute eAttribute, Literal geometry, boolean swapped)
+            throws EFeatureEncoderException {
+        super(
+                eAttribute,
+                SpatialConditionEncoder.contains(
+                        eAttribute.getEAttributeType(), geometry, swapped));
+    }
+
+    public EGeometryValueContains(EAttribute eAttribute, Object geometry, boolean swapped)
+            throws EFeatureEncoderException {
+        super(
+                eAttribute,
+                SpatialConditionEncoder.contains(
+                        eAttribute.getEAttributeType(), geometry, swapped));
+    }
+
+    public EGeometryValueContains(EAttribute eAttribute, Geometry geometry, boolean swapped)
+            throws EFeatureEncoderException {
+        super(
+                eAttribute,
+                SpatialConditionEncoder.contains(
+                        eAttribute.getEAttributeType(), geometry, swapped));
+    }
+}
