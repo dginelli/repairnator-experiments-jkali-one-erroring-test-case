4b352960e97c32457a393074b37886c5e40cf571
repairnator <noreply@github.com>
2020-03-19 19:42:08 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-wix-wix-embedded-mysql-237512170-20170530-153738_bugonly-firstCommit

---
+++ src/test/scala/com/wix/mysql/ParallelEmbeddedMysqlTest.scala
@@ -0,0 +1,32 @@
+package com.wix.mysql
+
+import java.util.concurrent.TimeUnit
+
+import com.wix.mysql.EmbeddedMysql._
+import com.wix.mysql.config.MysqldConfig.{SystemDefaults, aMysqldConfig}
+import com.wix.mysql.distribution.Version
+import com.wix.mysql.support.IntegrationTest
+import org.specs2.specification.AfterEach
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+class ParallelEmbeddedMysqlTest extends IntegrationTest {
+
+  "EmbeddedMysql instance" should {
+
+    "run 2 instances in parallel" in {
+      withCleanRepo {
+        Seq(runMysql(onPort = 3310), runMysql(onPort = 3311)).map(Await.result(_, 15 minutes)) must beSuccessful
+      }
+    }
+  }
+
+  def runMysql(onPort: Int) = Future {
+    val config = aMysqldConfig(Version.v5_7_latest).withPort(onPort).withTimeout(2, TimeUnit.MINUTES).build
+    val mysqld = start(anEmbeddedMysql(config))
+
+    mysqld must beAvailableOn(onPort, "auser", "sa", SystemDefaults.SCHEMA)
+  }
+}
