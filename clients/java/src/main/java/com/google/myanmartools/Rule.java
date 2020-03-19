4683c3a69a9fbb6a0a5e08861c316859e30c958c
repairnator <noreply@github.com>
2020-03-19 10:51:05 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-googlei18n-myanmar-tools-398468616-20180630-020110-firstCommit

---
+++ clients/java/src/main/java/com/google/myanmartools/Rule.java
@@ -0,0 +1,89 @@
+/* Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.myanmartools;
+
+import java.util.regex.Pattern;
+
+/**
+ * Class implementing a rule as part of a transliteration phase.
+ */
+
+class Rule {
+
+  // Fields of the rule
+  public Pattern pattern;
+  public String substitution;
+  public boolean matchOnStart = false;
+  public int revisitPosition = -1;
+  public String info = "";  // Id number or other information.
+  public String contextBefore = "";
+  public String contextAfter = "";
+
+  public Rule(String patternString, String substitutionString) {
+    pattern = Pattern.compile("^" + patternString);
+    substitution = substitutionString;
+  }
+
+  public Rule setInfo(String ruleInfo) {
+    info = ruleInfo;
+    return this;
+  }
+
+  public Rule setMatchOnStart() {
+    matchOnStart = true;
+    return this;
+  }
+
+  public Rule setRevisitPosition(int newPos) {
+    revisitPosition = newPos;
+    return this;
+  }
+
+  public Rule setBeforeContext(String before) {
+    contextBefore = before;
+    return this;
+  }
+
+  public Rule setAfterContext(String after) {
+    contextAfter = after;
+    return this;
+  }
+
+  public String printRule() {
+    String result = "    R " + info + " p: " + pattern + " s: " + substitution;
+    if (matchOnStart) {
+      result += " matchOnStart=True ";
+    }
+    if (revisitPosition >= 0) {
+      result += " revisitPosition= " + revisitPosition;
+    }
+
+    if (matchOnStart) {
+      result += " matchOnStart = true" ;
+    }
+
+    if (!"".equals(contextBefore)) {
+      result += " contextAfter = " + contextBefore ;
+    }
+
+    if (!"".equals(contextAfter)) {
+      result += " contextAfter = " + contextAfter ;
+    }
+
+    return result + "\n";
+  }
+
+}
