9b5a3373eba6cfb9edfdb9b8b35349a1156152bc
repairnator <noreply@github.com>
2020-03-19 11:10:16 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-openmrs-openmrs-module-attachments-359935323-20180329-185429-firstCommit

---
+++ api-2.0/src/test/java/org/openmrs/module/attachments/AttachmentsServiceTest.java
@@ -0,0 +1,75 @@
+package org.openmrs.module.attachments;
+
+import org.junit.Test;
+import org.openmrs.Encounter;
+import org.openmrs.Obs;
+import org.openmrs.Patient;
+import org.openmrs.Visit;
+import org.openmrs.api.EncounterService;
+import org.openmrs.api.PatientService;
+import org.openmrs.api.VisitService;
+import org.openmrs.module.attachments.obs.Attachment;
+import org.openmrs.module.attachments.obs.TestHelper;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AttachmentsServiceTest extends BaseModuleContextSensitiveTest {
+
+	@Autowired
+	private AttachmentsService as;
+
+	@Autowired
+	protected TestHelper testHelper;
+
+	@Autowired
+	@Qualifier("patientService")
+	protected PatientService ps;
+
+	@Autowired
+	@Qualifier("visitService")
+	protected VisitService vs;
+
+	@Autowired
+	@Qualifier("encounterService")
+	private EncounterService es;
+
+	@Test
+	public void getAttachments_shouldReturnAttachments() throws Exception {
+
+		// Setup
+		List<Obs> obsList = testHelper.saveComplexObs(3);
+
+		Obs obs = obsList.get(0);
+		Patient patient = obs.getEncounter().getPatient();
+		Visit visit = obs.getEncounter().getVisit();
+		Encounter encounter = obs.getEncounter();
+
+		// Replay
+		List<Attachment> attachmentsList = as.getAttachments(patient, visit, encounter, true);
+
+		// Verify
+		List<Attachment> originalAttachmentsList = obsList.stream().map(Attachment::new).collect(Collectors.toList());
+
+		assertEquals(new HashSet<>(originalAttachmentsList), new HashSet<>(attachmentsList));
+
+		for (Attachment originalAttachment : originalAttachmentsList) {
+			boolean found = false;
+			for (Attachment testingAttachment : attachmentsList) {
+				if (Objects.equals(originalAttachment.getUuid(), testingAttachment.getUuid())) {
+					found = true;
+					break;
+				}
+			}
+			assertTrue(found);
+		}
+	}
+}
